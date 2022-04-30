37380de643e59db06919f285565704d3bdbe280b
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-04-30 15:47:37 -0400

    Add contrast and selecting game object

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
-import { Grid } from '@nextui-org/react'
+import { Grid, Text } from '@nextui-org/react'
 import GameObjectsList from './components/GameObjectsList'
+import { useState } from 'react'
 
 function App() {
 
   const objects = ["GameCore", "Something", "Plant", "Really long name", "Gaming", "Mom", "Moo", "Cow", "Beep", "Beep", "Boat dog", "fern"] // .slice(0, 3)
 
+
+  const [selectedObject, setSelectedObject] = useState<string | undefined>(undefined)
 
   // future reference
   // 100vh means 100% of the view height
@@ -14,14 +17,17 @@ function App() {
     <div className="App">
       {/* Component data */}
       <Grid.Container>
-        <Grid xs>
+        <Grid xs css={{ backgroundColor: "$accents1" }}>
           <div className="center" style={{
             minHeight: '100vh',
             height: "100%",
             width: "100%",
+            display: "flex",
+            justifyContent: "center"
             // backgroundColor: "blue"
           }}>
 
+            <Text h1>{selectedObject ?? ""}</Text>
 
           </div>
         </Grid>
@@ -37,7 +43,7 @@ function App() {
             maxHeight: "100vh",
             // maxWidth: "30vw" // TODO: Figure out how to make overflow scroll horizontal work
           }}>
-            <GameObjectsList objects={objects} />
+            <GameObjectsList objects={objects} onSelect={(val) => setSelectedObject(val as string)} />
           </div>
         </Grid>
       </Grid.Container>
