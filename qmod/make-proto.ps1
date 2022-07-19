aa2ea6231fef3a36db0e5b521254ce4f92135af6
Metalit <48568911+Metalit@users.noreply.github.com>
2022-07-19 11:34:59 -0500

    Fix up generation

--- qmod/make-proto.ps1
+++ qmod/make-proto.ps1
@@ -4,4 +4,4 @@ if (Test-Path ".\protobuf\") {
 mkdir ./protobuf
 # & protoc -I="..\protos" --cpp_out="protobuf" ..\protos\qrue.proto
 # Use VCPKG protobuf for consistency
-& "$ENV:VCPKG_ROOT\installed\arm64-android\tools\protobuf\protoc.exe" --proto_path=../protos --cpp_out=./protobuf ../protos/qrue.proto
+& "$ENV:VCPKG_ROOT\installed\arm64-android\tools\protobuf\protoc.exe" --proto_path=../protos --cpp_out=./protobuf ../protos/unity.proto ../protos/il2cpp.proto ../protos/qrue.proto
