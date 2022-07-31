47bb958593aa0f20fc6379ba941679af72eade47
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-07-31 17:41:37 -0400

    Tab click and switching

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,29 +1,38 @@
 import { Divider } from "@nextui-org/react"
+import { useState } from "react";
 
 export interface TabsProps {
     tabs: Array<string>
-    selected: number
+    selected: number,
+    onTabSelected?: (name: string, i: number) => void
 }
 
 interface TabProps {
     name: string,
-    selected: boolean
+    selected: boolean,
+    onClick: () => void
 }
 
 function Tab(props: TabProps) {
     const activeClass = props.selected ? "tab-active" : "";
 
     return (
-        <a className={`tab tab-lifted ${activeClass}`}>{props.name}</a>
+        <a className={`tab tab-lifted ${activeClass}`} onClick={props.onClick}>{props.name}</a>
     )
 }
 
 export function Tabs(props: TabsProps) {
+    const [selected, setSelected] = useState(props.selected)
+
     return (
         <>
             <div className="tabs pt-2">
                 {props.tabs.map((n, i) => (
-                    <Tab key={n} name={n} selected={i === props.selected} />
+                    <Tab key={n} name={n} selected={i === selected} onClick={() => {
+                        setSelected(i);
+                        if (props.onTabSelected)
+                            props.onTabSelected(n, i);
+                    }} />
                 ))}
             </div>
             <Divider height={2} />
