37ca67e2348a18f3f40aed4ee3574c33d9a34e82
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-03-24 21:21:02 -0400

    Unused function

--- src/misc/utils.ts
+++ src/misc/utils.ts
@@ -1,29 +1,3 @@
-import { DependencyList, useEffect, useRef } from "react";
-
-export function useEffectAsync(
-    func: () => Promise<undefined | (() => void)>,
-    deps?: DependencyList,
-    error?: (reason: any) => void
-) {
-    const unregister = useRef<(() => void) | undefined>(undefined);
-
-    return useEffect(() => {
-        func()
-            .then((l) => (unregister.current = l))
-            .catch(
-                (e) =>
-                    (error && error(e)) ||
-                    console.error(`Caught error in useEffectAsync(): ${e}`)
-            );
-
-        return () => {
-            if (unregister.current) unregister.current();
-        };
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, deps);
-}
-
 export function uniqueNumber(min = 0, max = Number.MAX_SAFE_INTEGER) {
     return Math.floor(Math.random() * max + min);
 }
