f95e3c83c7c84d960a09f1b2910638c63dc4a4d5
Metalit <48568911+Metalit@users.noreply.github.com>
2023-05-02 11:15:05 -0500

    Fix some data sending

--- src/misc/utils.ts
+++ src/misc/utils.ts
@@ -7,41 +7,9 @@ export function uniqueNumber(min = 0, max = Number.MAX_SAFE_INTEGER) {
 
 function stringToBytes(input: string, typeInfo: PacketJSON<ProtoTypeInfo>) {
     let dataArray = new Uint8Array(typeInfo.size!);
-    if (typeInfo.primitiveInfo != undefined) {
-        switch (typeInfo.primitiveInfo) {
-        case ProtoTypeInfo.Primitive.BOOLEAN:
-            new DataView(dataArray.buffer).setUint8(0, Number(input == "true"));
-            break;
-        case ProtoTypeInfo.Primitive.CHAR:
-            new Uint16Array(dataArray.buffer)[0] = input.charCodeAt(0);
-            break;
-        case ProtoTypeInfo.Primitive.INT:
-            new DataView(dataArray.buffer).setInt32(0, Number(input));
-            break;
-        case ProtoTypeInfo.Primitive.LONG:
-            new DataView(dataArray.buffer).setBigInt64(0, BigInt(input));
-            break;
-        case ProtoTypeInfo.Primitive.FLOAT:
-            new DataView(dataArray.buffer).setFloat32(0, Number(input));
-            break;
-        case ProtoTypeInfo.Primitive.DOUBLE:
-            new DataView(dataArray.buffer).setFloat64(0, Number(input));
-            break;
-        case ProtoTypeInfo.Primitive.STRING:
-            console.log("conveting %s to bytes", input);
-            dataArray = new Uint8Array((input.length * 2) + 2);
-            const utf16Arr = new Uint16Array(dataArray.buffer);
-            for (let i = 0; i < input.length; i++)
-                utf16Arr[i] = input.charCodeAt(i);
-            console.log("result:", [...dataArray].join(","));
-            break;
-        case ProtoTypeInfo.Primitive.PTR:
-            new DataView(dataArray.buffer).setInt32(0, Number(input));
-            break;
-        case ProtoTypeInfo.Primitive.UNKNOWN:
-        case ProtoTypeInfo.Primitive.VOID:
-            break;
-        }
+    if (typeInfo.classInfo != undefined) {
+        const value = BigInt(input);
+        new DataView(dataArray.buffer).setBigInt64(0, value, true);
     } else if (typeInfo.structInfo != undefined) {
         const struct: {[key: string] : any} = JSON.parse(input);
         const fields = typeInfo.structInfo.fieldOffsets!;
@@ -51,9 +19,39 @@ function stringToBytes(input: string, typeInfo: PacketJSON<ProtoTypeInfo>) {
             string = string.replace(/^"|"$/g, '');
             dataArray.set(stringToBytes(string, field.type!), Number(offset));
         }
-    } else if (typeInfo.classInfo != undefined) {
-        const value = Number(input.substring(input.lastIndexOf("at ") + 3));
-        new DataView(dataArray.buffer).setInt32(0, value);
+    } else if (typeInfo.primitiveInfo != undefined) {
+        switch (typeInfo.primitiveInfo) {
+        case ProtoTypeInfo.Primitive.BOOLEAN:
+            new DataView(dataArray.buffer).setUint8(0, Number(input == "true"));
+            break;
+        case ProtoTypeInfo.Primitive.CHAR:
+            new Uint16Array(dataArray.buffer)[0] = input.charCodeAt(0);
+            break;
+        case ProtoTypeInfo.Primitive.INT:
+            new DataView(dataArray.buffer).setInt32(0, Number(input), true);
+            break;
+        case ProtoTypeInfo.Primitive.LONG:
+            new DataView(dataArray.buffer).setBigInt64(0, BigInt(input), true);
+            break;
+        case ProtoTypeInfo.Primitive.FLOAT:
+            new DataView(dataArray.buffer).setFloat32(0, Number(input), true);
+            break;
+        case ProtoTypeInfo.Primitive.DOUBLE:
+            new DataView(dataArray.buffer).setFloat64(0, Number(input), true);
+            break;
+        case ProtoTypeInfo.Primitive.STRING:
+            dataArray = new Uint8Array((input.length * 2) + 2);
+            const utf16Arr = new Uint16Array(dataArray.buffer);
+            for (let i = 0; i < input.length; i++)
+                utf16Arr[i] = input.charCodeAt(i);
+            break;
+        case ProtoTypeInfo.Primitive.PTR:
+            new DataView(dataArray.buffer).setBigInt64(0, BigInt(input), true);
+            break;
+        case ProtoTypeInfo.Primitive.UNKNOWN:
+        case ProtoTypeInfo.Primitive.VOID:
+            break;
+        }
     }
     return dataArray;
 }
@@ -72,7 +70,7 @@ function bytesToRealValue(bytes: DataView, typeInfo: PacketJSON<ProtoTypeInfo>, 
     const arr = new Uint8Array(bytes.buffer);
     console.log("bytes:", [...arr].join(","));
     if (typeInfo.classInfo != undefined) {
-        return `${typeInfo.classInfo.namespaze}::${typeInfo.classInfo.clazz} at ${bytes.getInt32(baseOffset).toString()}`;
+        return bytes.getBigInt64(baseOffset, true);
     } else if (typeInfo.structInfo != undefined) {
         let struct: {[key: string] : any} = {};
         const fields = typeInfo.structInfo.fieldOffsets!;
@@ -91,13 +89,13 @@ function bytesToRealValue(bytes: DataView, typeInfo: PacketJSON<ProtoTypeInfo>, 
             const byte = bytes.buffer.slice(baseOffset, baseOffset + 2);
             return new TextDecoder("utf-16").decode(byte);
         case ProtoTypeInfo.Primitive.INT:
-            return bytes.getInt32(baseOffset);
+            return bytes.getInt32(baseOffset, true);
         case ProtoTypeInfo.Primitive.LONG:
-            return bytes.getBigInt64(baseOffset);
+            return bytes.getBigInt64(baseOffset, true);
         case ProtoTypeInfo.Primitive.FLOAT:
-            return bytes.getFloat32(baseOffset);
+            return bytes.getFloat32(baseOffset, true);
         case ProtoTypeInfo.Primitive.DOUBLE:
-            return bytes.getFloat64(baseOffset);
+            return bytes.getFloat64(baseOffset, true);
         case ProtoTypeInfo.Primitive.STRING:
             let nullIndex = baseOffset;
             while (nullIndex < bytes.byteLength) {
@@ -108,7 +106,7 @@ function bytesToRealValue(bytes: DataView, typeInfo: PacketJSON<ProtoTypeInfo>, 
             const slice = bytes.buffer.slice(baseOffset, nullIndex);
             return new TextDecoder("utf-16").decode(slice);
         case ProtoTypeInfo.Primitive.PTR:
-            return bytes.getInt32(baseOffset);
+            return bytes.getBigInt64(baseOffset, true);
         case ProtoTypeInfo.Primitive.UNKNOWN:
             return "unknown";
         case ProtoTypeInfo.Primitive.VOID:
