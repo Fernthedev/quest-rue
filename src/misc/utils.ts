82a5f75f214049d8f844403d1dfae7f1eccb1478
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2024-02-26 19:10:07 -0400

    Add some useful comments

--- src/misc/utils.ts
+++ src/misc/utils.ts
@@ -1,15 +1,42 @@
 import toast from "solid-toast";
-import { Signal, SignalOptions, createEffect, createSignal, Accessor } from "solid-js";
+import {
+  Signal,
+  SignalOptions,
+  createEffect,
+  createSignal,
+  Accessor,
+  createRenderEffect,
+} from "solid-js";
 
+/**
+ * A signal that resets its value
+ * when its dependencies change
+ *
+ * Fern: I'm not even sure how this works
+ *
+ * @param val
+ * @param options
+ * @returns
+ */
 export function createUpdatingSignal<T>(
   val: () => T,
   options?: SignalOptions<T>
 ): Signal<T> {
   const [valAccessor, valSetter] = createSignal(val(), options);
-  // reset the value with dependencies
+  // reset the value when val is modified
   createEffect(() => valSetter(() => val())); // typescript is so stupid sometimes
   return [valAccessor, valSetter];
 }
+
+/**
+ * Stores the signal value in browser local storage
+ * USes the default value if none exists prior to use
+ *
+ * @param key
+ * @param defaultVal
+ * @param options
+ * @returns
+ */
 export function createPersistentSignal(
   key: string,
   defaultVal: () => string,
@@ -19,16 +46,35 @@ export function createPersistentSignal(
     localStorage.getItem(key) ?? defaultVal(),
     options
   );
+  // store the value every time val is modified
   createEffect(() => {
     localStorage.setItem(key, valAccessor());
   });
   return [valAccessor, valSetter];
 }
-// important: anything reactive used after an await will not be tracked
-export function createAsyncMemo<T>(val: () => Promise<T>): [Accessor<T | undefined>, () => Promise<T>] {
+
+/**
+ * Create a memo that resolves a promise or undefined if no value
+ *
+ * important: anything reactive used after an await will not be tracked
+ *
+ * @param valPromise
+ * @returns
+ */
+export function createAsyncMemo<T>(
+  valPromise: () => Promise<T>
+): [Accessor<T | undefined>, () => Promise<T>] {
+  // TODO: Use createResource or handle errors properly
   const [valAccessor, valSetter] = createSignal<T>();
-  const update = () => val().then((val) => valSetter(() => val));
-  createEffect(update);
+  const update = async () => {
+    // resolve promise before setter
+    const v = await valPromise();
+
+    return valSetter(() => v);
+  };
+  // run even if inital render phase
+  // we use effect to listen to changes
+  createRenderEffect(update);
   return [valAccessor, update];
 }
 
