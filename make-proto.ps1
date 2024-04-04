07640f4dc31176d4b2df66a56d9bc4a2276459e2
Metalit <48568911+Metalit@users.noreply.github.com>
2024-04-04 18:23:59 -0500

    Action change 12

--- make-proto.ps1
+++ make-proto.ps1
@@ -1,15 +1,11 @@
 # Use VCPKG protobuf for consistency
-if (Test-Path ".\qmod\protobuf\") {
+if (Test-Path ./qmod/protobuf/) {
     Remove-Item ./qmod/protobuf -Recurse -Confirm -Force
 }
 mkdir ./qmod/protobuf
 # & protoc -I="..\protos" --cpp_out="protobuf" ..\protos\qrue.proto
 # Use VCPKG protobuf for consistency
 
-# TODO: Make this work cross platform
-& "vcpkg_installed\arm64-android\tools\protobuf\protoc.exe" --proto_path=./protos `
-    --plugin=protoc-gen-ts_proto=.\node_modules\.bin\protoc-gen-ts_proto.cmd `
-    --ts_proto_opt=forceLong=bigint --ts_proto_opt=oneof=unions --ts_proto_opt=esModuleInterop=true `
-    --ts_proto_out=./src/misc/proto `
+& vcpkg_installed/arm64-android/tools/protobuf/protoc.exe --proto_path=./protos `
     --cpp_out=./qmod/protobuf `
     ./protos/qrue.proto ./protos/il2cpp.proto ./protos/unity.proto ./protos/paper.proto
