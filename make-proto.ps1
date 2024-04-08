a14f015c900bd8f99fe41f775108693b1a4f9aaa
Metalit <48568911+Metalit@users.noreply.github.com>
2024-04-08 11:07:50 -0500

    Action change 13

--- make-proto.ps1
+++ make-proto.ps1
@@ -1,11 +1,20 @@
+Param(
+    [Parameter(Mandatory=$false)]
+    [Switch] $y
+)
 # Use VCPKG protobuf for consistency
 if (Test-Path ./qmod/protobuf/) {
-    Remove-Item ./qmod/protobuf -Recurse -Confirm -Force
+    if ($y) {
+        Remove-Item ./qmod/protobuf -Recurse -Force
+    }
+    else {
+        Remove-Item ./qmod/protobuf -Recurse -Confirm -Force
+    }
 }
 mkdir ./qmod/protobuf
 # & protoc -I="..\protos" --cpp_out="protobuf" ..\protos\qrue.proto
 # Use VCPKG protobuf for consistency
 
-& vcpkg_installed/arm64-android/tools/protobuf/protoc.exe --proto_path=./protos `
+& vcpkg_installed/arm64-android/tools/protobuf/protoc --proto_path=./protos `
     --cpp_out=./qmod/protobuf `
     ./protos/qrue.proto ./protos/il2cpp.proto ./protos/unity.proto ./protos/paper.proto
